7ffd17c31317da18a3c29900e3871230ac3813b7
tanselbay1 <tanselbay1@hotmail.com>
2021-03-02 14:50:35 +0700

    only iteration problem left to solve

---
+++ script.js
@@ -0,0 +1,69 @@
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() *3) +1;
+    let choice = "";
+    switch (randomNumber) {
+        case 1:
+            choice = "rock";
+            break;
+        case 2:
+            choice = "paper";
+            break;
+        case 3:
+            choice = "scissors";
+    }
+    return choice;
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    let message = "";
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        message = "You lose! Paper beats Rock";
+        computerScore += 1;
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        message = "You win!! Rock beats Scissors";
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
+        message = "Tie! No points";
+    } else {
+        if (playerSelection == "paper" && computerSelection == "scissors") {
+            message = "You lose! Scissor beats Paper";
+            computerScore += 1;
+        } else if (playerSelection == "paper" && computerSelection == "rock") {
+            message = "You win!! Paper beats Rock";
+        } else if (playerSelection == "paper" && computerSelection == "paper") {
+            message = "Tie! No points";
+        } else {
+            if (playerSelection == "scissors" && computerSelection == "rock") {
+                message = "You lose! Rock beats Scissors";
+                computerScore += 1;
+            } else if (playerSelection == "scissors" && computerSelection == "paper") {
+                message = "You win!! Scissors beats Paper";
+            } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+                message = "Tie! No points";
+            } else {
+                message = "Something terrible happened!";
+            }
+        }
+    }
+    console.log(message);
+}
+
+function game() {
+    
+    for(let i = 0; i<5; i++) {
+        playRound(playerSelection, computerSelection);
+    }
+    if (playerScore > computerScore) {
+        return `You win! Your Score: ${playerScore} and Computer Score: ${computerScore}`;
+    } else if (playerScore < computerScore) {
+        return `You lose! Your Score: ${playerScore} and Computer Score: ${computerScore}`;
+    }
+}
+
+
+const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
+const computerSelection = computerPlay();
+
+console.log(game());
