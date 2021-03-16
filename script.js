b0748213227292e365b19f3121a1a1dc5c2e9c89
tanselbay1 <tanselbay1@hotmail.com>
2021-03-16 16:39:27 +0700

    Logic upgraded and DOM functions added

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// VARIABLES
+
+let playerScore = 0;
+let computerScore = 0;
+
+const buttonContainer = document.getElementById('buttonContainer');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const results = document.getElementById('results');
+const winner = document.getElementById('winner');
+const message = document.getElementById('message');
+const restartButton = document.getElementById('reset');
+
+
+// FUNCTIONS
+
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() *3) +1;
     let choice = "";
@@ -14,62 +31,79 @@ function computerPlay() {
     return choice;
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
 function playRound(playerSelection, computerSelection) {
-    let message = "";
     if (playerSelection == "rock" && computerSelection == "paper") {
-        message = "You lose! Paper beats Rock";
+        message.innerText = "You lose! Paper beats Rock";
         computerScore += 1;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        message = "You win!! Rock beats Scissors";
+        message.innerText = "You win!! Rock beats Scissors";
         playerScore += 1;
     } else if (playerSelection == "rock" && computerSelection == "rock") {
-        message = "Tie! No points";
+        message.innerText = "Tie! No points";
     } else {
         if (playerSelection == "paper" && computerSelection == "scissors") {
-            message = "You lose! Scissor beats Paper";
+            message.innerText = "You lose! Scissor beats Paper";
             computerScore += 1;
         } else if (playerSelection == "paper" && computerSelection == "rock") {
-            message = "You win!! Paper beats Rock";
+            message.innerText = "You win!! Paper beats Rock";
             playerScore += 1;
         } else if (playerSelection == "paper" && computerSelection == "paper") {
-            message = "Tie! No points";
+            message.innerText = "Tie! No points";
         } else {
             if (playerSelection == "scissors" && computerSelection == "rock") {
-                message = "You lose! Rock beats Scissors";
+                message.innerText = "You lose! Rock beats Scissors";
                 computerScore += 1;
             } else if (playerSelection == "scissors" && computerSelection == "paper") {
-                message = "You win!! Scissors beats Paper";
+                message.innerText = "You win!! Scissors beats Paper";
                 playerScore += 1;
             } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-                message = "Tie! No points";
+                message.innerText = "Tie! No points";
             } else {
-                message = "Please enter Rock, Paper or Scissors!";
+                message.innerText = "Please enter Rock, Paper or Scissors!";
             }
         }
     }
     return message;
 }
 
-function game() {
-    
-    for(let i = 0; i<5; i++) {           
-        const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        
-    }
-
-    if (playerScore > computerScore) {
-        return `You win! Your Score: ${playerScore} and Computer Score: ${computerScore}`;
-    } else if (playerScore < computerScore) {
-        return `You lose! Your Score: ${playerScore} and Computer Score: ${computerScore}`;
-    } else {
-        return `Tie! Your Score: ${playerScore} and Computer Score: ${computerScore}`;
-    }
+function gameRestart() {
+    playerScore = 0;
+    computerScore = 0;
+    winner.innerText = '';
+    results.innerText = `Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+    message.innerText = "Game Started Again!";
 }
 
 
-console.log(game());
+// EVENTS
+
+buttonContainer.addEventListener('click', function(e){
+    let playerSelection = '';
+    switch (e.target) {
+        case rock:
+            playerSelection = 'rock';
+            break;
+        case paper:
+            playerSelection = 'paper';
+            break;
+        case scissors:
+            playerSelection = 'scissors';
+            break;
+        default:
+            console.log('not working');
+            break;
+    }
+    // console.log(playRound(playerSelection, computerPlay())); Keep this for testing purpose.
+    playRound(playerSelection, computerPlay());
+    results.innerText = `Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+    if (playerScore >= 5){
+        winner.innerText = "Congratulations, you WIN!";
+        setTimeout(gameRestart, 2000);
+    } else if (computerScore >= 5) {
+        winner.innerText = "You LOSE!!";
+        setTimeout(gameRestart, 2000);
+    }
+    // console.log(results); Again, for testing.
+})
+
+restartButton.addEventListener('click', gameRestart);
